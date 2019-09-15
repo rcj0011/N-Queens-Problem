045983bcb8dcea778267562132e9831e8763fea3
camjewell <camjewell50@gmail.com>
2019-09-15 18:16:41 -0500

    Defined Queen class.

--- HW1.1.py
+++ HW1.1.py
@@ -1,15 +1,32 @@
-def run():
-    solveNQueens(25)
-
 def solveNQueens(n):
     print (n)
+    numQueens = 0
+    initialState = []
+    queensList = []
+
+    for i in range(n):
+        initialState.append(i+1)
+        q = Queen(i, i+1)
+        queensList.append(q)
 
 def scoreFunction():
     score = 0
 
-    
+
 
     return score
 
+def getNeighbors(queen):
+
+    return None
+
+class Queen():
+    column = 0
+    row = 0
+
+    def __init__(self, row, col):
+        self.row = row
+        self.column = col
+
 if __name__ == "__main__":
-    run()
+    solveNQueens(25)
