fc598728fc0ceddf9057f38bb010082f799a65e1
camjewell <camjewell50@gmail.com>
2019-09-15 18:43:37 -0500

    Polished scoreFunction.

--- HW1.1.py
+++ HW1.1.py
@@ -1,18 +1,27 @@
+queensList = []
+
 def solveNQueens(n):
     print (n)
-    numQueens = 0
     initialState = []
-    queensList = []
 
     for i in range(n):
         initialState.append(i+1)
-        q = Queen(i, i+1)
+        q = Queen(i+1, i+1)
         queensList.append(q)
+    
+    scoreFunction()
 
 def scoreFunction():
     score = 0
 
-
+    for q in queensList:
+        for Q in queensList:
+            if q.col != Q.col and (q.row == Q.row or q.col-Q.col == q.row-Q.row) and Q not in q.conflicts: #absolute value
+                score += 1
+                q.conflicts.append(Q)
+    
+    for q in queensList:
+        q.resetConflicts()
 
     return score
 
@@ -21,12 +30,16 @@ def getNeighbors(queen):
     return None
 
 class Queen():
-    column = 0
+    col = 0
     row = 0
+    conflicts = []
 
     def __init__(self, row, col):
         self.row = row
-        self.column = col
+        self.col = col
+
+    def resetConflicts(self):
+        self.conflicts = []
 
 if __name__ == "__main__":
     solveNQueens(25)
